f095be76ff26f62144b050e48521cb35673baacb
Zion Emond <zion.emond@cqlcorp.com>
2025-10-14 15:46:43 -0400

    init commit

---
+++ test/hogwarts-notification-service.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HogwartsNotificationService from '../lib/hogwarts-notification-service-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hogwarts-notification-service-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HogwartsNotificationService.HogwartsNotificationServiceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
