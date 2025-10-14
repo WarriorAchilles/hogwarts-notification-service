d8a3886bd3504434a0132236237940cc4735fa07
Zion Emond <zion.emond@cqlcorp.com>
2025-10-14 19:58:20 -0400

    project skeleton

--- lib/hogwarts-notification-service-stack.ts
+++ lib/hogwarts-notification-service-stack.ts
@@ -1,5 +1,7 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigw from "aws-cdk-lib/aws-apigateway";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class HogwartsNotificationServiceStack extends cdk.Stack {
@@ -12,5 +14,45 @@ export class HogwartsNotificationServiceStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'HogwartsNotificationServiceQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    const code = lambda.Code.fromAsset("src");
+
+    // lambda function resource
+    const myFunction = new lambda.Function(this, "HelloWorldFunction", {
+      runtime: lambda.Runtime.NODEJS_22_X,
+      handler: "lambda/index.handler",
+      code: code,
+    });
+
+    // lambda function url resource
+    const myFunctionUrl = myFunction.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+    });
+
+    // CloudFormation output for the url
+    new cdk.CfnOutput(this, "myFunctionUrlOutput", {
+      value: myFunctionUrl.url,
+    });
+
+    // API DEFINITION
+    const api = new apigw.RestApi(this, "MyApiGateway", {
+      restApiName: "MyServiceApi",
+      deployOptions: {
+        stageName: "dev",
+      },
+    });
+
+    const notificationsResource = api.root.addResource("notifications");
+    const notificationByRecipient =
+      notificationsResource.addResource("{recipient}");
+    notificationByRecipient.addMethod(
+      "GET",
+      new apigw.LambdaIntegration(myFunction)
+    );
+
+    notificationsResource.addMethod(
+      "POST",
+      new apigw.LambdaIntegration(myFunction)
+    );
   }
 }
