d908162df9821ddb403909373de875bb2b3f6c55
Zion Emond <zion.emond@cqlcorp.com>
2025-10-15 17:11:39 -0400

    stub code

--- src/lambda/processNotification.ts
+++ src/lambda/processNotification.ts
@@ -1,16 +1,42 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { SecretsManager } from "aws-sdk";
+import * as mysql from "mysql2/promise";
 
-export const handler = async (
-  event: APIGatewayProxyEvent
-): Promise<APIGatewayProxyResult> => {
+const DB_HOST = process.env.DB_HOST || "";
+const DB_SECRET = process.env.DB_SECRET || "";
+
+export const handler = async (event: APIGatewayProxyEvent): Promise<void> => {
   console.log("Event received:", JSON.stringify(event, null, 2));
 
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      message: "process notification lambda",
-      input: event,
-    }),
-  };
+  // receive message from queue
+  const message = "placeholder";
+
+  // log message to simulate message being sent
+
+  // update message record in database
+  const secretsManager = new SecretsManager();
+  const secretData = await secretsManager
+    .getSecretValue({ SecretId: DB_SECRET })
+    .promise();
+  const { username, password } = JSON.parse(secretData.SecretString!);
+  console.log(`got the db secrets. User: ${username}, Pass: ${password}`);
+
+  const connection = await mysql.createConnection({
+    host: DB_HOST,
+    user: username,
+    password: password,
+    multipleStatements: true,
+    database: "HogwartsDb",
+  });
+  console.log("successfully connected to db: ", connection);
+
+  try {
+    // TODO: update this to correct SQL
+    const x = await connection.execute(
+      `SELECT * FROM Notifications WHERE id EQUALS ${message}`
+    );
+    console.log("sucessfully updated message in database");
+  } catch (e) {
+    console.error(`Error interacting with DB: ${e}`);
+  }
 };
--- src/lambda/retrieveNotifications.ts
+++ src/lambda/retrieveNotifications.ts
@@ -1,16 +1,50 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { SecretsManager } from "aws-sdk";
+import * as mysql from "mysql2/promise";
+
+const DB_HOST = process.env.DB_HOST || "";
+const DB_SECRET = process.env.DB_SECRET || "";
 
 export const handler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
   console.log("Event received:", JSON.stringify(event, null, 2));
 
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      message: "get notifications lambda",
-      input: event,
-    }),
-  };
+  const recipient = "placeholder";
+
+  const secretsManager = new SecretsManager();
+  const secretData = await secretsManager
+    .getSecretValue({ SecretId: DB_SECRET })
+    .promise();
+  const { username, password } = JSON.parse(secretData.SecretString!);
+  console.log(`got the db secrets. User: ${username}, Pass: ${password}`);
+
+  const connection = await mysql.createConnection({
+    host: DB_HOST,
+    user: username,
+    password: password,
+    multipleStatements: true,
+    database: "HogwartsDb",
+  });
+  console.log("successfully connected to db: ", connection);
+
+  try {
+    const x = await connection.execute(
+      `SELECT * FROM Notifications WHERE recipient EQUALS ${recipient}`
+    );
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        message: x,
+        input: event,
+      }),
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      headers: { "Content-Type": "application/json" },
+      body: `Error interacting with DB: ${e}`,
+    };
+  }
 };
