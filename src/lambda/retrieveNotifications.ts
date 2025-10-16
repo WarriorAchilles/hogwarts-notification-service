5f9006c1a4eae6f6f8c3fd632bfa1d886480fe61
Zion Emond <zion.emond@cqlcorp.com>
2025-10-15 23:52:48 -0400

    get notifications by recipient

--- src/lambda/retrieveNotifications.ts
+++ src/lambda/retrieveNotifications.ts
@@ -9,27 +9,31 @@ export const handler = async (
 ): Promise<APIGatewayProxyResult> => {
   console.log("Event received:", JSON.stringify(event, null, 2));
 
-  const recipient = "placeholder";
-  const db = await getDb(DB_HOST, DB_SECRET);
+  if (event.pathParameters && event.pathParameters.recipient) {
+    const recipient = decodeURIComponent(event.pathParameters.recipient);
+    const db = await getDb(DB_HOST, DB_SECRET);
 
-  try {
-    // TODO if I have time: guard against SQL injection
-    const x = await db.execute(
-      `SELECT * FROM Notifications WHERE recipient EQUALS ${recipient}`
-    );
-    return {
-      statusCode: 200,
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        message: x,
-        input: event,
-      }),
-    };
-  } catch (e) {
-    return {
-      statusCode: 500,
-      headers: { "Content-Type": "application/json" },
-      body: `Error interacting with DB: ${e}`,
-    };
+    try {
+      // TODO if I have time: guard against SQL injection
+      const items = await db.execute(
+        `SELECT * FROM Notifications WHERE recipient = '${recipient}'`
+      );
+      return {
+        statusCode: 200,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(items[0], null, 2),
+      };
+    } catch (e) {
+      return {
+        statusCode: 500,
+        headers: { "Content-Type": "application/json" },
+        body: `Error interacting with DB: ${e}`,
+      };
+    }
   }
+  return {
+    statusCode: 400,
+    headers: { "Content-Type": "application/json" },
+    body: "No recipient path parameter specified.",
+  };
 };
